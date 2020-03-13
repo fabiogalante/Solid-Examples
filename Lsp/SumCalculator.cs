13e19fb7ad45186f1a1d831a0f0c26746b476b5c
Galante <fabiogalantemans@gmail.com>
2020-03-13 12:38:28 -0300

    LCP - 1

---
+++ Lsp/SumCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace Lsp
+{
+    public class SumCalculator
+    {
+        protected readonly int[] _numbers;
+
+        public SumCalculator(int[] numbers)
+        {
+            _numbers = numbers;
+        }
+
+        public int Calculate()
+        {
+            return _numbers.Sum();
+        }
+    }
+}
